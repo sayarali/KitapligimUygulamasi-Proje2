503bbdb72671fac89235da78ee309779b026b912
Ali Sayar <alisayar07@gmail.com>
2022-04-04 20:22:00 +0300

    Initial commit

---
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.alisayar.kitapligimuygulamas_proje2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alisayar.kitapligimuygulamas_proje2.login.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
+
