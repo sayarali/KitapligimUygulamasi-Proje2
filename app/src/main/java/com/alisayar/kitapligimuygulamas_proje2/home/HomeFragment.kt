3ade0b30124ad9b581688ecf22f43733142a1a61
Ali Sayar <alisayar07@gmail.com>
2022-06-04 22:39:57 +0300

    NavDrawer fotoğrafı, fab eklendi

--- app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/home/HomeFragment.kt
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/home/HomeFragment.kt
@@ -1,24 +1,40 @@
 package com.alisayar.kitapligimuygulamas_proje2.home
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.alisayar.kitapligimuygulamas_proje2.R
+import com.alisayar.kitapligimuygulamas_proje2.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
 
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fab.setOnClickListener {
+
+        }
+
     }
 
 
---
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/home/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.alisayar.kitapligimuygulamas_proje2.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+    init {
+
+    }
+
+}
