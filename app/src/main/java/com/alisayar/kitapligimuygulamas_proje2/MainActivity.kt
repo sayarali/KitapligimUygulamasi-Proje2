b0a6a212e6608a70d48cc14de3c5122faba8baee
Ali Sayar <alisayar07@gmail.com>
2022-06-04 20:38:17 +0300

    NavigationDrawer yapıldı çıkış yap butonu çalışmıyor bakılacak

--- app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/MainActivity.kt
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/MainActivity.kt
@@ -3,22 +3,65 @@ package com.alisayar.kitapligimuygulamas_proje2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.alisayar.kitapligimuygulamas_proje2.databinding.ActivityMainBinding
 import com.alisayar.kitapligimuygulamas_proje2.login.LoginActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
         if(auth.currentUser == null){
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
+        setupNavigation()
+
     }
+
+    override fun onSupportNavigateUp() = navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
+
+    private fun setupNavigation() {
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setSupportActionBar(binding.toolbar)
+
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+        binding.navigationView.setupWithNavController(navController)
+
+
+
+
+
+        navController.addOnDestinationChangedListener{_, destination: NavDestination, _ ->
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
 }
 
 
---
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/profile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.alisayar.kitapligimuygulamas_proje2.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alisayar.kitapligimuygulamas_proje2.R
+
+
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
