89e14a09a803b80fcad276ce43bc94b589a64e51
Ali Sayar <alisayar07@gmail.com>
2022-06-04 16:40:09 +0300

    Giriş işlemleri tamamen bitti ve çalışır durumda

--- app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/login/signin/SignInFragment.kt
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/login/signin/SignInFragment.kt
@@ -1,13 +1,17 @@
 package com.alisayar.kitapligimuygulamas_proje2.login.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.alisayar.kitapligimuygulamas_proje2.MainActivity
 import com.alisayar.kitapligimuygulamas_proje2.R
 import com.alisayar.kitapligimuygulamas_proje2.databinding.FragmentSignInBinding
 
@@ -40,5 +44,26 @@ class SignInFragment : Fragment() {
             val action = R.id.action_signInFragment_to_signUpFragment
             findNavController().navigate(action)
         }
+
+        viewModel.emailError.observe(viewLifecycleOwner, Observer {
+            binding.signinEmailTil.error = it
+        })
+
+        viewModel.passwordError.observe(viewLifecycleOwner, Observer {
+            binding.signinPasswordTil.error = it
+        })
+
+        viewModel.loginSuccessEvent.observe(viewLifecycleOwner, Observer {
+            if(it){
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
+                viewModel.loginEventComplete()
+            }
+        })
+
+        viewModel.toastMessage.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        })
     }
 }
--- app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/login/signin/SignInViewModel.kt
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/login/signin/SignInViewModel.kt
@@ -1,6 +1,7 @@
 package com.alisayar.kitapligimuygulamas_proje2.login.signin
 
 import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -11,19 +12,48 @@ class SignInViewModel: ViewModel() {
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
 
+    private val _emailError = MutableLiveData<String>()
+    val emailError: LiveData<String> get() = _emailError
+    private val _passwordError = MutableLiveData<String>()
+    val passwordError: LiveData<String> get() = _passwordError
+
+    private val _loginSuccessEvent = MutableLiveData<Boolean>()
+    val loginSuccessEvent: LiveData<Boolean> get() = _loginSuccessEvent
+
+    private val _toastMessage = MutableLiveData<String>()
+    val toastMessage: LiveData<String> get() = _toastMessage
 
     init {
         auth = FirebaseAuth.getInstance()
     }
 
     fun signIn() {
-        auth.signInWithEmailAndPassword(email.value.toString(), password.value.toString()).addOnCompleteListener {
-            if (it.isSuccessful){
-                println("Giriş Yapıldı")
+        var isError = false
+        if(email.value.toString() == "" || email.value == null){
+            _emailError.value = "E-posta boş bırakılamaz."
+            isError = true
+        } else
+            _emailError.value = ""
+        if(password.value.toString() == "" || password.value == null){
+            _passwordError.value = "Parola boş bırakılamaz."
+            isError = true
+        } else
+            _passwordError.value = ""
+
+        if(!isError){
+            auth.signInWithEmailAndPassword(email.value.toString(), password.value.toString()).addOnCompleteListener {
+                if (it.isSuccessful){
+                    _loginSuccessEvent.value = true
+                }
+            }.addOnFailureListener {
+                _toastMessage.value = it.localizedMessage
             }
-        }.addOnFailureListener {
-            println(it.localizedMessage)
         }
+
+    }
+
+    fun loginEventComplete(){
+        _loginSuccessEvent.value = false
     }
 
 }
