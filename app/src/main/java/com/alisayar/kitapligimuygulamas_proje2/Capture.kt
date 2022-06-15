79670a1441b8938573a146f2932f87f38f54e601
Ali Sayar <alisayar07@gmail.com>
2022-06-15 07:21:35 +0300

    Barcode Scanner

---
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/Capture.kt
@@ -0,0 +1,7 @@
+package com.alisayar.kitapligimuygulamas_proje2
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class Capture: CaptureActivity() {
+
+}
--- app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/add/AddFragment.kt
+++ app/src/main/java/com/alisayar/kitapligimuygulamas_proje2/add/AddFragment.kt
@@ -1,15 +1,23 @@
 package com.alisayar.kitapligimuygulamas_proje2.add
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.alisayar.kitapligimuygulamas_proje2.Capture
 import com.alisayar.kitapligimuygulamas_proje2.R
 import com.alisayar.kitapligimuygulamas_proje2.databinding.FragmentAddBinding
-import com.alisayar.kitapligimuygulamas_proje2.databinding.FragmentHomeBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import java.lang.Exception
 
 
 class AddFragment : Fragment() {
@@ -62,9 +70,38 @@ class AddFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.barcode_item){
-            TODO("Barkod tarama fonksiyonu")
+
+            barcodeScanner()
+
         }
         return super.onOptionsItemSelected(item)
     }
 
+
+
+    private fun barcodeScanner(){
+        val intentIntegrator = IntentIntegrator.forSupportFragment(this)
+        intentIntegrator.setPrompt("Flaş aç -> Ses açma tuşu\n\nFlaş kapat -> Ses kapatma tuşu")
+        intentIntegrator.setBeepEnabled(false)
+        intentIntegrator.setOrientationLocked(true)
+        intentIntegrator.captureActivity = Capture::class.java
+        intentIntegrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == IntentIntegrator.REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK && data != null){
+                try {
+                    val intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+                    if(intentResult != null){
+                        binding.addEt.setText(intentResult.contents)
+                    }
+                } catch (e: Exception){}
+            }
+        }
+
+
+    }
 }
